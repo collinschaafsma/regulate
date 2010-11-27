e4b16bcdd0b386a16ad47511ff7c02cd91e0576f
Collin Schaafsma <collin@quickleft.com>
2010-11-27 10:19:01 -0700

    adding basic spec helper to require our rails dummy app

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+# Configure Rails Envinronment
+ENV["RAILS_ENV"] = "test"
+
+require "dummy/config/environment"
+require "rails/test_help"
+require "rspec/rails"
+
+ActionMailer::Base.delivery_method = :test
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.default_url_options[:host] = "test.com"
+
+Rails.backtrace_cleaner.remove_silencers!
+
+Rspec.configure do |config|
+  # Remove this line if you don't want Rspec's should and should_not
+  # methods or matchers
+  require 'rspec/expectations'
+  config.include Rspec::Matchers
+  # == Mock Framework
+  config.mock_with :rspec
+end
