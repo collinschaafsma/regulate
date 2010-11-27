90cf453b57a95eedf19723989f4d0fc2ff9badff
Collin Schaafsma <collin@quickleft.com>
2010-11-27 10:06:24 -0700

    adding some basic structure for testing and a dummy rails app to test against

---
+++ README.md
@@ -0,0 +1,5 @@
+#Regulate
+
+Simple CMS which allows you to control what regions of a page can be managed.
+
+Work in progress, check back soon.
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '65dd253620e33d84a20428dbcc18e8aad01dece187114ecab665a3fd9738bfedae056015634c662677ec3294b81ddf93ae65910b4815e982e4812c76bd35db24'
